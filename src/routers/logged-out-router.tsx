dd8eafead5ddcc6eb1bf357a38cda44b581f53ca
genhcode99 <genhcode99@gmail.com>
2021-08-15 08:47:38 +0900

    #15.1 React Hook Form

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,15 +1,55 @@
 import React from "react"
+import { useForm } from "react-hook-form"
 import { isLoggedInVar } from "../apollo"
 
+// * 컴포넌트
 export const LoggedOutRouter = () => {
-  const onClick = () => {
-    isLoggedInVar(true)
+  // * 기능
+  // react hook form
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = () => {
+    console.log(watch())
   }
 
+  const onInvalid = () => {
+    console.log("cant create account")
+  }
+
+  console.log(errors)
+
+  // * 화면출력
   return (
     <div>
       <h1>Logged Out</h1>
-      <button onClick={onClick}>Click to Login</button>
+      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
+        <div>
+          <input
+            {...register("email", {
+              required: "this is required",
+              pattern: /^[A-Za-z0-9._%+-]+@gmail.com$/,
+            })}
+            name="email"
+            type="email"
+            placeholder="email"
+          />
+        </div>
+        <div>
+          <input
+            {...register("password", { required: true })}
+            name="password"
+            type="password"
+            required
+            placeholder="password"
+          />
+        </div>
+        <button className="bg-yellow-300 text-white">Submit</button>
+      </form>
     </div>
   )
 }
