224941b5d0c4c91ec45e580c914e1f89507b8cd7
genhcode99 <genhcode99@gmail.com>
2021-08-14 18:46:57 +0900

    #15.0 Local Only Fields

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,6 +1,21 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client"
+import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client"
+
+export const isLoggedInVar = makeVar(false)
 
 export const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    // graphql을 가지고 client로 요청했을 때 정보를 주는 방법
+    typePolicies: {
+      Query: {
+        fields: {
+          isLoggedIn: {
+            read() {
+              return isLoggedInVar()
+            },
+          },
+        },
+      },
+    },
+  }),
 })
