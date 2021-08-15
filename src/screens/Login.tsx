f2ed2593a505ea171d55735c8473bca654ecfd09
genhcode99 <genhcode99@gmail.com>
2021-08-15 11:06:52 +0900

    #15.4 Form Design

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,7 +1,26 @@
 import React from "react"
 
 const Login = () => {
-  return <div>login</div>
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-800">
+      <div className="bg-white w-full max-w-lg py-10 rounded-md text-center">
+        <h3 className="text-3xl text-gray-800">Log In</h3>
+        <form className="flex flex-col mt-5 px-5">
+          <input
+            placeholder="Email"
+            className="bg-gray-100 shadow-inner mb-3 py-3 px-5 rounded-md focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600"
+          />
+          <input
+            placeholder="Password"
+            className="bg-gray-100 shadow-inner py-3 px-5 rounded-md focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 "
+          />
+          <button className="bg-gray-800 py-3 px-5 text-white font-medium text-lg rounded-md mt-3 focus:outline-none hover:opacity-90">
+            Log In
+          </button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default Login
