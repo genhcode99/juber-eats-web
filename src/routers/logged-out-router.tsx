eed0aade9f789c0c4c541aa97c518c4efaaad6f1
genhcode99 <genhcode99@gmail.com>
2021-08-15 10:14:40 +0900

    #15.3 Router and @types

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,64 +1,20 @@
 import React from "react"
-import { useForm } from "react-hook-form"
-import { isLoggedInVar } from "../apollo"
-
-interface IForm {
-  email: string
-  password: string
-}
+import Login from "../screens/Login"
+import CreateAccount from "../screens/CreateAccount"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 // * 컴포넌트
 export const LoggedOutRouter = () => {
-  // * 기능
-  // react hook form
-  const {
-    register,
-    watch,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<IForm>()
-  const onSubmit = () => {
-    console.log(watch())
-  }
-  const onInvalid = () => {
-    console.log("cant create account")
-  }
-
-  // * 화면출력
   return (
-    <div>
-      <h1>Logged Out</h1>
-      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
-        <div>
-          <input
-            {...register("email", {
-              required: "Email Is Required",
-              pattern: /^[A-Za-z0-9._%+-]+@gmail.com$/,
-            })}
-            name="email"
-            type="email"
-            placeholder="email"
-          />
-          {errors.email?.message && (
-            <span className=" font-bold text-red-600">
-              {errors.email?.message}
-            </span>
-          )}
-          {errors.email?.type === "pattern" && (
-            <span className=" font-bold text-red-600">Only Gamil Allowed</span>
-          )}
-        </div>
-        <div>
-          <input
-            {...register("password", { required: true })}
-            name="password"
-            type="password"
-            required
-            placeholder="password"
-          />
-        </div>
-        <button className="bg-yellow-300 text-white">Submit</button>
-      </form>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/create-account">
+          <CreateAccount />
+        </Route>
+        <Route path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
