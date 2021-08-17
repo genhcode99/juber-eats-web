3dc21119cc63aa9960ad170b017215ce0e47fa0a
genhcode99 <genhcode99@gmail.com>
2021-08-17 11:02:57 +0900

    #15.18 Header part Two

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,21 @@
+import { faUser } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import React from "react"
+import { Link } from "react-router-dom"
+import { useMe } from "../hooks/useMe"
 import juberLogo from "../images/Logo.svg"
-const Header = () => {
+
+const Header: React.FC = () => {
+  const { data } = useMe()
   return (
     <header className="py-4">
-      <div className="w-full max-w-screen-xl mx-auto">
-        <img src={juberLogo} alt="Juber Eats" className="w-40 mb-10" />
+      <div className="w-full px-5 xl:px-0 max-w-screen-xl mx-auto flex justify-between items-center">
+        <img src={juberLogo} alt="Juber Eats" className="w-24" />
+        <span className="text-xs">
+          <Link to="/my-profile">
+            <FontAwesomeIcon icon={faUser} className=" text-xl" />
+          </Link>
+        </span>
       </div>
     </header>
   )
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,14 +1,13 @@
 import React from "react"
-import { gql, useQuery } from "@apollo/client"
-import { meQuery } from "../graphql_type/meQuery"
 import {
   BrowserRouter as Router,
   Redirect,
   Route,
   Switch,
 } from "react-router-dom"
-import Restaurants from "../screens/client/Restaurants"
+import { useMe } from "../hooks/useMe"
 import Header from "../components/Header"
+import Restaurants from "../screens/client/Restaurants"
 
 // <==========( Route )==========>
 const ClientRoutes = [
@@ -17,23 +16,10 @@ const ClientRoutes = [
   </Route>,
 ]
 
-// <==========( Graphql )==========>
-const ME_QUERY = gql`
-  query meQuery {
-    me {
-      id
-      email
-      role
-      verified
-    }
-  }
-`
-// <==========( 설정 )==========>
-
 // <==========( 컴포넌트 )==========>
 export const LoggedInRouter = () => {
   // <==========( 기능 )==========>
-  const { data, loading, error } = useQuery<meQuery>(ME_QUERY)
+  const { data, loading, error } = useMe()
 
   // <==========( Loading 화면 출력 )==========>
   if (!data || loading || error) {
