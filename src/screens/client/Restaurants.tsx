c17999dcf14f8cdad892e7b0b851511cb2d03a91
genhcode99 <genhcode99@gmail.com>
2021-08-17 21:20:57 +0900

    #17.2 Restaurants List

--- src/screens/client/Restaurants.tsx
+++ src/screens/client/Restaurants.tsx
@@ -1,4 +1,5 @@
 import { gql, useQuery } from "@apollo/client"
+import { url } from "inspector"
 import React from "react"
 import {
   restaurantsPageQuery,
@@ -59,16 +60,30 @@ export const Restaurants = () => {
       </form>
       <div>
         {!loading && (
-          <div className="max-w-screen-2xl mx-auto mt-8">
+          <div className="max-w-screen-2xl mx-auto mt-8 px-5">
             <div className=" max-w-md mx-auto flex justify-around">
               {data?.allCategories.categories?.map((category) => (
-                <div className="flex flex-col items-center">
+                <div className="flex flex-col items-center cursor-pointer group">
                   <div
-                    className="w-14 h-14 rounded-full bg-cover hover:bg-gray-100 cursor-pointer"
+                    className="w-16 h-16 rounded-full bg-cover group-hover:bg-gray-200 "
                     style={{ backgroundImage: `url(${category.coverImg})` }}
                   ></div>
                   <span className="text-sm text-center font-medium mt-1">
                     {category.name}
+                  </span>
+                </div>
+              ))}
+            </div>
+            <div className="grid grid-cols-3 gap-x-5 gap-y-10 mt-10">
+              {data?.allRestaurants.results?.map((restaurant) => (
+                <div>
+                  <div
+                    className="bg-red-500 py-28 bg-cover bg-center mb-3"
+                    style={{ backgroundImage: `url(${restaurant.coverImg})` }}
+                  ></div>
+                  <h3 className="text-xl font-medium">{restaurant.name}</h3>
+                  <span className="border-t-2 border-gray-200">
+                    {restaurant.category?.name}
                   </span>
                 </div>
               ))}
