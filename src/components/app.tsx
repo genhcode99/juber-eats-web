f6fb632459a6d79dbdd62f2caa316f64a188a529
genhcode99 <genhcode99@gmail.com>
2021-08-18 20:03:45 +0900

    #18.0 Tests Setup

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,14 +1,12 @@
 import React from "react"
-import { isLoggedInVar } from "./apollo"
+import { isLoggedInVar } from "../apollo"
 import { useReactiveVar } from "@apollo/client"
-import { LoggedInRouter } from "./routers/logged-in-router"
-import { LoggedOutRouter } from "./routers/logged-out-router"
+import { LoggedInRouter } from "../routers/logged-in-router"
+import { LoggedOutRouter } from "../routers/logged-out-router"
 
 // Component
-function App() {
+export const App = () => {
   const isLoggedIn = useReactiveVar(isLoggedInVar)
 
   return isLoggedIn ? <LoggedInRouter /> : <LoggedOutRouter />
 }
-
-export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import App from "./App"
+import { App } from "./components/app"
 import "./index.css"
 import React from "react"
 import ReactDOM from "react-dom"
