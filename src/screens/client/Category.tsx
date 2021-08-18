4546fcb3d18e75b5cb29f82f94a3c1cef96166b4
genhcode99 <genhcode99@gmail.com>
2021-08-18 17:30:37 +0900

    #17.7 Code Challenge

--- src/screens/client/Category.tsx
+++ src/screens/client/Category.tsx
@@ -1,11 +1,12 @@
-import { gql, useQuery } from "@apollo/client"
-import React, { useEffect } from "react"
+import React, { useState } from "react"
 import { useParams } from "react-router-dom"
-import { CATEGORY_FRAGMENT, RESTAURANT_FRAGMENT } from "../../fragments"
 import {
   findCategoryBySlug,
   findCategoryBySlugVariables,
 } from "../../graphql_type/findCategoryBySlug"
+import { gql, useQuery } from "@apollo/client"
+import { CATEGORY_FRAGMENT, RESTAURANT_FRAGMENT } from "../../fragments"
+import { Restaurant } from "../../components/Restaurant"
 
 // <==========( GraphQl )==========>
 const CATEGORY_QUERY = gql`
@@ -35,12 +36,58 @@ interface IPatamsType {
 // <==========( Feature )==========>
 export const Category = () => {
   const { slug } = useParams<IPatamsType>()
+  const [page, setPage] = useState(1)
+
   const { data, loading } = useQuery<
     findCategoryBySlug,
     findCategoryBySlugVariables
-  >(CATEGORY_QUERY, { variables: { input: { page: 1, slug } } })
+  >(CATEGORY_QUERY, { variables: { input: { page, slug } } })
 
-  console.log(data?.findCategoryBySlug)
+  const categoryName = data?.findCategoryBySlug.category?.name
+
+  // pagination
+  const onClickNext = () => setPage((current) => current + 1)
+  const onClickPrev = () => setPage((current) => current - 1)
+
   // <==========( Presenter )==========>
-  return <div>hi</div>
+  return (
+    <div className="max-w-screen-2xl mx-auto mt-8 px-5 pb-20">
+      <div className="max-w-screen-2xl grid md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-10 mt-16">
+        {data?.findCategoryBySlug.restaurants?.map((restaurant) => (
+          <Restaurant
+            key={restaurant.id}
+            id={restaurant.id + ""}
+            coverImg={`url(${restaurant.coverImg})`}
+            name={restaurant.name}
+            categoryName={categoryName}
+          />
+        ))}
+      </div>
+      <div className="grid grid-cols-3 items-center max-w-md mx-auto mt-10">
+        {page > 1 ? (
+          <button
+            onClick={onClickPrev}
+            className="font-medium text-2xl focus-within:outline-none"
+          >
+            &larr;
+          </button>
+        ) : (
+          <div></div>
+        )}
+        <span>
+          Page {page} of {data?.findCategoryBySlug.totalPages}
+        </span>
+        {page !== data?.findCategoryBySlug.totalPages ? (
+          <button
+            onClick={onClickNext}
+            className="font-medium text-2xl focus-within:outline-none"
+          >
+            &rarr;
+          </button>
+        ) : (
+          <div></div>
+        )}
+      </div>
+    </div>
+  )
 }
--- src/screens/client/Search.tsx
+++ src/screens/client/Search.tsx
@@ -1,7 +1,8 @@
 import { gql, useLazyQuery } from "@apollo/client"
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { Helmet } from "react-helmet-async"
 import { useHistory, useLocation } from "react-router-dom"
+import { Restaurant } from "../../components/Restaurant"
 import { RESTAURANT_FRAGMENT } from "../../fragments"
 import {
   searchRestaurant,
@@ -26,6 +27,9 @@ const SEARCH_RESTAURANT = gql`
 
 // <==========( Feature )==========>
 export const Search = () => {
+  // state
+  const [page, setPage] = useState(1)
+
   // hooks
   const location = useLocation()
   const history = useHistory()
@@ -42,18 +46,58 @@ export const Search = () => {
       return history.replace("/")
     }
     searchRestaurantQuery({
-      variables: { input: { page: 1, query } },
+      variables: { input: { page, query } },
     })
   }, [history, location.search])
-  console.log(loading, data, called)
+
+  // pagination
+  const onClickNext = () => setPage((current) => current + 1)
+  const onClickPrev = () => setPage((current) => current - 1)
 
   // <==========( Presenter )==========>
   return (
-    <div>
-      <Helmet>
-        <title>Home | Juber Eats</title>
-      </Helmet>
-      Search
+    <div className="max-w-screen-2xl mx-auto mt-8 px-5 pb-20">
+      <div className="max-w-screen-2xl grid md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-10 mt-16">
+        {data?.searchRestaurant.restaurants &&
+          data?.searchRestaurant.restaurants.map((restaurant) => (
+            <Restaurant
+              key={restaurant.id}
+              id={restaurant.id + ""}
+              coverImg={`url(${restaurant.coverImg})`}
+              name={restaurant.name}
+              categoryName={restaurant.category?.name}
+            />
+          ))}
+      </div>
+      {data?.searchRestaurant.restaurants?.length !== 0 ? (
+        <div className="grid grid-cols-3 items-center max-w-md mx-auto mt-10">
+          {page > 1 ? (
+            <button
+              onClick={onClickPrev}
+              className="font-medium text-2xl focus-within:outline-none"
+            >
+              &larr;
+            </button>
+          ) : (
+            <div></div>
+          )}
+          <span>
+            Page {page} of {data?.searchRestaurant.totalPages}
+          </span>
+          {page !== data?.searchRestaurant.totalPages ? (
+            <button
+              onClick={onClickNext}
+              className="font-medium text-2xl focus-within:outline-none"
+            >
+              &rarr;
+            </button>
+          ) : (
+            <div></div>
+          )}
+        </div>
+      ) : (
+        <h2 className="text-center">Restaurant Not Found</h2>
+      )}
     </div>
   )
 }
