ecf7f76a5c2f0f44d628066c3143cb1f81037a53
genhcode99 <genhcode99@gmail.com>
2021-08-23 16:30:01 +0900

    #19.2 Login E2E

--- cypress/integration/first-test.ts
+++ cypress/integration/first-test.ts
@@ -1,23 +1,34 @@
 describe("Log In", () => {
-  it("should see login page", () => {
-    cy.visit("/").title().should("eq", "Login | Juber Eats")
-  })
+  const user = cy
 
-  it("can fill out the form", () => {
-    cy.visit("/")
-      .findByPlaceholderText(/email/i)
-      .type("test@email.com")
-      .findByPlaceholderText(/password/i)
-      .type("testPassword")
-      .findByRole("button")
-      .should("not.have.class", "pointer-events-none")
+  it("should see login page", () => {
+    user.visit("/").title().should("eq", "Login | Juber Eats")
   })
 
   it("can see email / password validation errors", () => {
-    cy.visit("/")
-      .findByPlaceholderText(/email/i)
-      .type("bad@email")
+    user.visit("/")
+    user.findByPlaceholderText(/email/i).type("bad@email")
+    user
       .findByRole("alert")
       .should("have.text", "The email format is not valid.")
+    user.findByPlaceholderText(/email/i).clear()
+    user.findByRole("alert").should("have.text", "Email Is Required")
+    user.findByPlaceholderText(/email/i).type("test@email.com")
+    user
+      .findByPlaceholderText(/password/i)
+      .type("a")
+      .clear()
+    user.findByRole("alert").should("have.text", "Password Is Required")
+  })
+
+  it("can fill out the form", () => {
+    user.visit("/")
+    user.findByPlaceholderText(/email/i).type("Client@email.com")
+    user.findByPlaceholderText(/password/i).type("123")
+    user
+      .findByRole("button")
+      .should("not.have.class", "pointer-events-none")
+      .click()
+    user.window().its("localStorage.juber-token").should("be.a", "string ")
   })
 })
