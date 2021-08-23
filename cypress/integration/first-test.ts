71d4d31e87a147105006a75f7f7d43a9f3859551
genhcode99 <genhcode99@gmail.com>
2021-08-23 15:44:15 +0900

    #19.1 Our First Cypress Test

--- cypress/integration/first-test.ts
+++ cypress/integration/first-test.ts
@@ -1,5 +1,23 @@
-describe("First Test", () => {
-  it("should go to homepage", () => {
-    cy.visit("http://localhost:3000").title().should("eq", "Login | Juber Eats")
+describe("Log In", () => {
+  it("should see login page", () => {
+    cy.visit("/").title().should("eq", "Login | Juber Eats")
+  })
+
+  it("can fill out the form", () => {
+    cy.visit("/")
+      .findByPlaceholderText(/email/i)
+      .type("test@email.com")
+      .findByPlaceholderText(/password/i)
+      .type("testPassword")
+      .findByRole("button")
+      .should("not.have.class", "pointer-events-none")
+  })
+
+  it("can see email / password validation errors", () => {
+    cy.visit("/")
+      .findByPlaceholderText(/email/i)
+      .type("bad@email")
+      .findByRole("alert")
+      .should("have.text", "The email format is not valid.")
   })
 })
