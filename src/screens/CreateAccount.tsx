db760efa92f009bbb1d8e4dcb1f4754134e16d75
genhcode99 <genhcode99@gmail.com>
2021-08-23 12:30:47 +0900

    #18.9 CreateAccount Tests part Two

--- src/screens/CreateAccount.tsx
+++ src/screens/CreateAccount.tsx
@@ -13,7 +13,7 @@ import {
 } from "../graphql_type/createAccountMutation"
 
 // <==========( GraphQl )==========>
-const CREATE_ACCOUNT_MUTATION = gql`
+export const CREATE_ACCOUNT_MUTATION = gql`
   mutation createAccountMutation($createAccountInput: CreateAccountInput!) {
     createAccount(input: $createAccountInput) {
       ok
--- src/screens/__test__/CreateAccount.spec.tsx
+++ src/screens/__test__/CreateAccount.spec.tsx
@@ -1,8 +1,10 @@
 import React from "react"
-import CreateAccount from "../CreateAccount"
+import CreateAccount, { CREATE_ACCOUNT_MUTATION } from "../CreateAccount"
 import { ApolloProvider } from "@apollo/client"
+import userEvent from "@testing-library/user-event"
 import { render, waitFor, RenderResult } from "../../test-utils"
 import { createMockClient, MockApolloClient } from "mock-apollo-client"
+import { UserRole } from "../../graphql_type/globalTypes"
 
 describe("<CreateAccount />", () => {
   let mockedClient: MockApolloClient
@@ -22,5 +24,76 @@ describe("<CreateAccount />", () => {
     await waitFor(() => {
       expect(document.title).toBe("Create Account | Juber Eats")
     })
+  })
+
+  it("renders validation errors", async () => {
+    const { getByRole, getByPlaceholderText, debug } = renderResult
+    const email = getByPlaceholderText(/email/i)
+    const password = getByPlaceholderText(/password/i)
+    const button = getByRole("button")
+
+    await waitFor(() => {
+      userEvent.type(email, "wont@work")
+    })
+    let errorMessage = getByRole("alert")
+    expect(errorMessage).toHaveTextContent(/The email format is not valid./i)
+
+    await waitFor(() => {
+      userEvent.clear(email)
+    })
+    errorMessage = getByRole("alert")
+    expect(errorMessage).toHaveTextContent(/Email Is Required/i)
+
+    await waitFor(() => {
+      userEvent.type(email, "test@email.com")
+      userEvent.click(button)
+    })
+    errorMessage = getByRole("alert")
+    expect(errorMessage).toHaveTextContent(/Password Is Required/i)
+  })
+
+  it("submits mutaion with form values", async () => {
+    const { getByRole, getByPlaceholderText, debug } = renderResult
+    const email = getByPlaceholderText(/email/i)
+    const password = getByPlaceholderText(/password/i)
+    const button = getByRole("button")
+
+    const formData = {
+      email: "test@email.com",
+      password: "testPassword",
+      role: UserRole.Client,
+    }
+
+    const mockedLoginMutationResponse = jest.fn().mockResolvedValue({
+      data: {
+        createAccount: {
+          ok: true,
+          error: "mutation-error",
+        },
+      },
+    })
+    mockedClient.setRequestHandler(
+      CREATE_ACCOUNT_MUTATION,
+      mockedLoginMutationResponse,
+    )
+
+    jest.spyOn(window, "alert").mockImplementation(() => null)
+
+    await waitFor(() => {
+      userEvent.type(email, formData.email)
+      userEvent.type(password, formData.password)
+      userEvent.click(button)
+    })
+
+    expect(mockedLoginMutationResponse).toHaveBeenCalledTimes(1)
+    expect(mockedLoginMutationResponse).toHaveBeenCalledWith({
+      createAccountInput: {
+        ...formData,
+      },
+    })
+
+    expect(window.alert).toHaveBeenCalledWith("Account Created! Log in now!")
+    const mutationError = getByRole("alert")
+    expect(mutationError).toHaveTextContent("mutation-error")
   })
 })
