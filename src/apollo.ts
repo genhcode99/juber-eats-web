f0175017aa256410c23317d357f2c91bf2fafe9b
genhcode99 <genhcode99@gmail.com>
2021-08-27 18:04:27 +0900

    #23.1 Subscription Setup

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,16 +1,30 @@
 import {
+  split,
+  makeVar,
   ApolloClient,
   InMemoryCache,
-  makeVar,
   createHttpLink,
 } from "@apollo/client"
-import { setContext } from "@apollo/client/link/context"
 import { LOCALSTORAGE_TOKEN } from "./constants"
+import { WebSocketLink } from "@apollo/client/link/ws"
+import { setContext } from "@apollo/client/link/context"
+import { getMainDefinition } from "@apollo/client/utilities"
 
 const token = localStorage.getItem(LOCALSTORAGE_TOKEN)
 
 export const isLoggedInVar = makeVar(Boolean(token))
 export const authTokenVar = makeVar(token)
+
+// Websocket Link를 만듦
+const wsLink = new WebSocketLink({
+  uri: "ws://localhost:4000/graphql",
+  options: {
+    reconnect: true,
+    connectionParams: {
+      "x-jwt": authTokenVar() || "",
+    },
+  },
+})
 
 // Http Link 를 만듦
 const httpLink = createHttpLink({
@@ -27,11 +41,21 @@ const authLink = setContext((_, { headers }) => {
   }
 })
 
-// Link를 합쳐준다.
-const finalLink = authLink.concat(httpLink)
+// Http Link 를 사용할지 Websocket Link 를 사용할지 Query(subscription) 에 따라 선택
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    )
+  },
+  wsLink,
+  authLink.concat(httpLink), //헤더를 설정한 http 링크
+)
 
 export const client = new ApolloClient({
-  link: finalLink,
+  link: splitLink,
   cache: new InMemoryCache({
     // graphql을 가지고 client로 요청했을 때 정보를 주는 방법
     typePolicies: {
