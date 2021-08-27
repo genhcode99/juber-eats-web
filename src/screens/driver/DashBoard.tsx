c937d8c6618da556bf68d5748749331612798081
genhcode99 <genhcode99@gmail.com>
2021-08-27 22:16:54 +0900

    #23.6 Driver Dashboard part Two

--- src/screens/driver/DashBoard.tsx
+++ src/screens/driver/DashBoard.tsx
@@ -1,8 +1,40 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import GoogleMapReact from "google-map-react"
+
+// <==========( Settings )==========>
+interface ICoords {
+  latitude: number
+  longitude: number
+}
 
 // <==========( Feature )==========>
 export const DashBoard = () => {
+  const [driverCoords, setDriverCoords] = useState<ICoords>({
+    latitude: 0,
+    longitude: 0,
+  })
+
+  // <위도,경도 가져오기>
+  const onSucces = ({
+    coords: { latitude, longitude },
+  }: GeolocationPosition) => {
+    setDriverCoords({ latitude, longitude })
+  }
+  const onError = (error: GeolocationPositionError) => {
+    console.log(error)
+  }
+  useEffect(() => {
+    navigator.geolocation.watchPosition(onSucces, onError, {
+      enableHighAccuracy: true,
+    })
+  }, [])
+
+  const onApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
+    setTimeout(() => {
+      map.panTo(new maps.LatLng(driverCoords.latitude, driverCoords.longitude))
+    }, 5000)
+  }
+
   // <==========( Presenter )==========>
   return (
     <div>
@@ -11,12 +43,12 @@ export const DashBoard = () => {
         style={{ width: window.innerWidth, height: "95vh" }}
       >
         <GoogleMapReact
-          defaultZoom={20}
-          defaultCenter={{ lat: 59.95, lng: 30.33 }}
+          defaultZoom={15}
+          defaultCenter={{ lat: 36.31, lng: 126.88 }}
+          yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={onApiLoaded}
           bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP! }}
-        >
-          <h1>hello</h1>
-        </GoogleMapReact>
+        ></GoogleMapReact>
       </div>
     </div>
   )
